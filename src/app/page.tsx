0088e3fabba4964e93dd3bdcd920299a975fb525
Ryan Hang <ryan.hang3r@gmail.com>
2025-07-20 03:39:07 -0700

    03:trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-const Page = () => {
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Ryan PREFETCH"}));
+
 
   return (
-    <div> 
-      Vibe Check
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
