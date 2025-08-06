f79039b6a7b0438c770ca34fd0daea372bbcab50
Ryan Hang <ryan.hang3r@gmail.com>
2025-08-05 19:42:11 -0700

    19: bug fixes

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const ErrorPage = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">Something went wrong, please try again.</h1>
+        </div>
+    )
+}
--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -2,6 +2,7 @@ import { ProjectView } from "@/modules/projects/ui/views/project-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface Props {
     params: Promise <{
@@ -21,9 +22,11 @@ const Page = async ({ params }: Props) => {
   }));
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProjectView projectId={projectId} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something went wrong</p>}>
+        <Suspense fallback={<div>Loading...</div>}>
+            <ProjectView projectId={projectId} />
+        </Suspense>
+      </ErrorBoundary>
     </HydrationBoundary>
   )
 }
